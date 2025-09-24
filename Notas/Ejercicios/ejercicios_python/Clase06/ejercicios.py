92c86c16f048301c8744ecfb3f94ae26d540c463
nacho garcía <chonamiguelgarcia@gmail.com>
2025-09-24 20:00:39 -0300

    continuación clase 6

--- Notas/Ejercicios/ejercicios_python/Clase06/ejercicios.py
+++ Notas/Ejercicios/ejercicios_python/Clase06/ejercicios.py
@@ -1,17 +1,34 @@
 import random
 
-def tirar():
-    return [ random.randint(1,6) for i in range(5) ]
+def tirar(cant_dados):
+    return [ random.randint(1,6) for i in range(cant_dados) ]
 
 def es_generala(tirada):
     return len(tirada) == len(set(tirada))
 
-def prob_genarala(N):
-    
+def mas_repetido(dados):
+    cant = []
+    for i in range(1,7):
+        cant.append(dados.count(i)) 
+    maximo = max(cant)
+    num_repetido = cant.index(maximo)
+    return num_repetido + 1
 
+def jugar_mano():
+    tirada = tirar(6)
+    for i in range(2):
+        if es_generala(tirada):
+            return True
+        repetido = mas_repetido(tirada)
+        dados_guardados = [d for d in tirada if d == repetido]
+        dados_nuevos = tirar(6 - len(dados_guardados))
+        tirada = dados_guardados + dados_nuevos
+    return es_generala(tirada)
 
-N = 1000000
-G = sum([es_generala(tirar()) for i in range(N)])
-prob = G/N
-print(f'Tiré {N} veces, de las cuales {G} saqué generala servida.')
-print(f'Podemos estimar la probabilidad de sacar generala servida mediante {prob:.6f}.')
+def prog_generala(N):
+    return N
+
+tirada = jugar_mano()
+print(tirada)
+cantidades = mas_repetido(tirada)
+print(cantidades)
