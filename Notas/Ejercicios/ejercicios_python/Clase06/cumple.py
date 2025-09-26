5f216c2912a2740313fdbbeba2440f40d75f6666
nacho garcía <chonamiguelgarcia@gmail.com>
2025-09-26 17:31:12 -0300

    seguí con la clase 6, cumple + monte carlo

---
+++ Notas/Ejercicios/ejercicios_python/Clase06/cumple.py
@@ -0,0 +1,23 @@
+import random
+
+def tirar():
+    return random.randint(1,365)
+
+def cumplen_dos(grupo_de_personas):
+    return len(set(grupo_de_personas)) < len(grupo_de_personas)
+
+def grupo_de_personas(n):
+    grupo = [ tirar() for _ in range(n) ]
+    return grupo
+
+def simulación_cum(N, tamaño_grupo):
+    G = sum([cumplen_dos(grupo_de_personas(tamaño_grupo)) for _ in range(N)])
+    prob = G/N
+    return prob
+
+
+N = 100000
+for i in range(1, 80):
+    if simulación_cum(N, i) > 0.5:
+        print("Para que la probabilidad de que dos personas cumplan el mismo día sea mayor al 50% se necesita al menos un grupo de " + str(i) + " personas")
+        break
